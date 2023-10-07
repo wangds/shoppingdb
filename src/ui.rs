49f2ac110bb24fa550817b1d9ea63bd0940cc321
Sarah Wang <millimillenary@gmail.com>
2023-10-08 09:34:49 +1100

    Add stylised key bar

--- src/ui.rs
+++ src/ui.rs
@@ -1,5 +1,18 @@
 use ratatui::{prelude::*, widgets::*};
 
+const KEY_BAR_ITEMS: &[(&'static str, &'static str)] = &[
+    (" 1", "Help"),
+    (" 2", "    "),
+    (" 3", "    "),
+    (" 4", "    "),
+    (" 5", "    "),
+    (" 6", "    "),
+    (" 7", "    "),
+    (" 8", "    "),
+    (" 9", "    "),
+    ("10", "Quit"),
+];
+
 pub fn render_tui<B: Backend>(frame: &mut Frame<B>) {
     let layout = Layout::default()
         .direction(Direction::Vertical)
@@ -14,10 +27,16 @@ pub fn render_tui<B: Backend>(frame: &mut Frame<B>) {
 }
 
 pub fn render_key_bar<B: Backend>(frame: &mut Frame<B>, layout: Rect) {
-    let text = vec![
-        Span::styled("10", Style::default().fg(Color::White).bg(Color::Black)),
-        Span::styled("Quit", Style::default().fg(Color::Black).bg(Color::Cyan)),
-    ];
+    let key_style = Style::default().fg(Color::White).bg(Color::Black);
+    let text_style = Style::default().fg(Color::Black).bg(Color::Cyan);
 
-    frame.render_widget(Paragraph::new(Line::from(text)), layout);
+    let div = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![Constraint::Percentage(10)].repeat(KEY_BAR_ITEMS.len()))
+        .split(layout);
+
+    for (i, &(key, text)) in KEY_BAR_ITEMS.iter().enumerate() {
+        let hint = vec![Span::styled(key, key_style), Span::from(text)];
+        frame.render_widget(Paragraph::new(Line::from(hint)).style(text_style), div[i]);
+    }
 }
